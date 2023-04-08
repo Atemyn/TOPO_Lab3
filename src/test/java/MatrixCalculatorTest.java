b574cf57b65cdbef5c09a0270332a61159073ddb
Atemyn <atemyn@mail.ru>
2023-04-08 21:22:59 +0700

    Создан тест для создания класса матричного калькулятора

--- src/test/java/MatrixCalculatorTest.java
+++ src/test/java/MatrixCalculatorTest.java
@@ -1,2 +1,10 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class MatrixCalculatorTest {
+    @Test
+    void matrixCalculatorCreationTest() {
+        MatrixCalculator matrixCalculator = new MatrixCalculator();
+        Assertions.assertNotNull(matrixCalculator);
+    }
 }
