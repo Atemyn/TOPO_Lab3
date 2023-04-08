b4863e5fd3e4deb743508e29551de026e56cd37c
Atemyn <atemyn@mail.ru>
2023-04-08 21:46:21 +0700

    Добавлен Javadoc для метода сложения двух матриц

--- src/main/java/MatrixCalculator.java
+++ src/main/java/MatrixCalculator.java
@@ -1,5 +1,12 @@
 public class MatrixCalculator {
+    /**
+     * Метод для сложения двух матриц
+     * @param matrix1 первый аргумент, матрица, содержащая вещественные числа
+     * @param matrix2 второй аргумент, матрица, содержащая вещественные числа
+     * @return результат сложения двух аргументов
+     */
     public double[][] add(double[][] matrix1, double[][] matrix2) {
+        // TODO Реализовать метод сложения матриц до конца.
         return new double[0][0];
     }
 }
