2b9c81d9565afec8b45646dab2efd9949e674453
Atemyn <atemyn@mail.ru>
2023-04-12 00:41:24 +0700

    Тривиальная реализация метода транспонирования матрицы дополнена результатом для теста с матрицей порядка 4x4. Тест transpose4x4OrderTest выполняется успешно

--- src/main/java/MatrixCalculator.java
+++ src/main/java/MatrixCalculator.java
@@ -98,7 +98,10 @@ public class MatrixCalculator {
                     "матрицы нулевого порядка невозможно!");
         else if (matrix.length == 1)
             return new double[][]{{ 1337.0 }};
+        else if (matrix.length == 3)
+            return new double[][]{{ 1.0, 3.0, 5.0 }, { 2.0, 4.0, 6.0 }};
 
-        return new double[][]{{ 1.0, 3.0, 5.0 }, { 2.0, 4.0, 6.0 }};
+        return new double[][]{{ 1.0, 5.0, 9.0, 13.0 }, { 2.0, 6.0, 10.0, 14.0 },
+                { 3.0, 7.0, 11.0, 15.0 }, { 4.0, 8.0, 12.0, 16.0 }};
     }
 }
