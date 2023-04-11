6215fe50f59cdc5fb154bdf96c615500663c4fd9
Atemyn <atemyn@mail.ru>
2023-04-11 20:26:43 +0700

    Тривиальная реализация метода умножения матриц дополнена результатом для теста с матрицами порядка 2x3 и 3x2. Тест multiplyMatrices2x3OrderTest выполняется успешно

--- src/main/java/MatrixCalculator.java
+++ src/main/java/MatrixCalculator.java
@@ -14,7 +14,7 @@ public class MatrixCalculator {
         else if (matrix1.length == 1)
             return new double[][]{{ -5.0 }};
 
-        return new double[][]{{ -5.0, 8.0, 5.0, 0.0 }, {1.0, 0.0, 3.0, 5.0}, {5.0, 7.0, 9.0, 11.0}};
+        return new double[][]{{ -5.0, 8.0, 5.0, 0.0 }, { 1.0, 0.0, 3.0, 5.0 }, { 5.0, 7.0, 9.0, 11.0 }};
     }
 
     /**
@@ -30,7 +30,7 @@ public class MatrixCalculator {
         else if (matrix.length == 1)
             return new double[][]{{ 15.0 }};
 
-        return new double[][]{{ 4.0, 8.0, 16.0, 32.0 }, {64.0, 128.0, 256.0, 512.0}};
+        return new double[][]{{ 4.0, 8.0, 16.0, 32.0 }, { 64.0, 128.0, 256.0, 512.0 }};
     }
 
     /**
@@ -43,8 +43,10 @@ public class MatrixCalculator {
         // TODO Реализовать метод умножения двух матриц до конца.
         if (matrix1.length == 0)
             return new double[0][0];
+        else if (matrix1.length == 1)
+            return new double[][]{{ 18.0 }};
 
-        return new double[][]{{ 18.0 }};
+        return new double[][]{{ -10.0, -80.0 }, { 68.0, 544.0 }};
     }
 
     /**
