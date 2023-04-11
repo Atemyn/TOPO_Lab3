def24c5dd1ad16cbfafc99bb2818c27738dc657c
Atemyn <atemyn@mail.ru>
2023-04-11 20:00:18 +0700

    Создан тест с матрицами порядка 3x4 для метода сложения матриц

--- src/test/java/MatrixCalculatorTest.java
+++ src/test/java/MatrixCalculatorTest.java
@@ -27,6 +27,17 @@ public class MatrixCalculatorTest {
     }
 
     @Test
+    void addMatrices3x4OrderTest() {
+        MatrixCalculator matrixCalculator = new MatrixCalculator();
+        double[][] firstMatrix = {{ 5.0, 2.0, 1.5, 7.89 }, {9.0, -6.57, 2.5, 1.7}, {1.0, 2.0, 3.0, 4.0}},
+                secondMatrix = {{ -10.0, 6.0, 3.5, -7.89 }, {-8.0, 6.57, 0.5, 3.3}, {4.0, 5.0, 6.0, 7.0}},
+                resultMatrix = {{ -5.0, 8.0, 5.0, 0.0 }, {1.0, 0.0, 3.0, 5.0}, {5.0, 7.0, 9.0, 11.0}};
+
+        Assertions.assertArrayEquals(resultMatrix,
+                matrixCalculator.add(firstMatrix, secondMatrix));
+    }
+
+    @Test
     void multiplyMatrixZeroOrderByNumberTest() {
         MatrixCalculator matrixCalculator = new MatrixCalculator();
         Assertions.assertArrayEquals(new double[0][0],
