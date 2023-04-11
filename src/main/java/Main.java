e55cb794e39e05f5c7c046e335ee9a4187400762
Atemyn <atemyn@mail.ru>
2023-04-11 21:56:50 +0700

    Написан метод для ввода матрицы из терминала

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,8 +3,10 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
+    private static final BufferedReader reader =
+            new BufferedReader(new InputStreamReader(System.in));
     public static void main(String[] args) throws IOException {
-        var reader = new BufferedReader(new InputStreamReader(System.in));
+
         var matrixCalculator = new MatrixCalculator();
         int operation = 0;
 
@@ -20,5 +22,16 @@ public class Main {
             operation = Integer.parseInt(reader.readLine());
 
         } while (operation != 0);
+    }
+
+    private double[][] readMatrix(int rowsCount, int columnsCount) throws IOException {
+        double[][] matrix = new double[rowsCount][columnsCount];
+        for (int i = 0; i < rowsCount; i++) {
+            for (int j = 0; j < columnsCount; j++) {
+                System.out.format("¬ведите элемент матрицы под номером [%d][%d]: ", i + 1, j + 1);
+                matrix[i][j] = Double.parseDouble(reader.readLine());
+            }
+        }
+        return matrix;
     }
 }
